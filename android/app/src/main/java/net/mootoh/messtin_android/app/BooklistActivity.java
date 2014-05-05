35d07bf3f4d9ed11e57ab2d4ce0add027c39d221
Motohiro Takayama <mootoh@gmail.com>
2014-05-05 09:03:13 +0900

    wip: successfully access to Google Drive through SDK, but only in the scope of SCOPE_FILE...

--- android/app/src/main/java/net/mootoh/messtin_android/app/BooklistActivity.java
+++ android/app/src/main/java/net/mootoh/messtin_android/app/BooklistActivity.java
@@ -2,17 +2,29 @@ package net.mootoh.messtin_android.app;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi;
+import com.google.android.gms.drive.DriveFolder;
+import com.google.android.gms.drive.Metadata;
+import com.google.android.gms.drive.MetadataBuffer;
 
 class ImageAdapter extends BaseAdapter {
     private Context mContext;
@@ -54,6 +66,8 @@ class ImageAdapter extends BaseAdapter {
 }
 
 public class BooklistActivity extends Activity {
+    final private static int RESOLVE_CONNECTION_REQUEST_CODE = 1;
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,8 +83,91 @@ public class BooklistActivity extends Activity {
                 Toast.makeText(BooklistActivity.this, "" + position, Toast.LENGTH_SHORT).show();
             }
         });
+
+        final Activity self = this;
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Drive.API)
+                .addScope(Drive.SCOPE_FILE)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(Bundle bundle) {
+                        Log.d("@@@", "onConneted");
+                        listUpFiles();
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int i) {
+                        Log.d("@@@", "onConnetionSuspended");
+                    }
+                })
+                .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(ConnectionResult connectionResult) {
+                        Log.d("@@@", "onConnetionFailed: " + connectionResult.toString());
+                        if (connectionResult.hasResolution()) {
+                            try {
+                                connectionResult.startResolutionForResult(self, RESOLVE_CONNECTION_REQUEST_CODE);
+                            } catch (IntentSender.SendIntentException e) {
+                                // Unable to resolve, message user appopriately
+                            }
+                        } else {
+                            GooglePlayServicesUtil.getErrorDialog(connectionResult.getErrorCode(), self, 0).show();
+                        }
+                    }
+                })
+                .build();
     }
 
+    private void listUpFiles() {
+        DriveFolder rootFolder = Drive.DriveApi.getRootFolder(mGoogleApiClient);
+
+        rootFolder.listChildren(mGoogleApiClient).setResultCallback(new ResultCallback<DriveApi.MetadataBufferResult>() {
+            @Override
+            public void onResult(DriveApi.MetadataBufferResult result) {
+                if (! result.getStatus().isSuccess()) {
+                    Log.d("@@@", "failed in retrieving messtin dir");
+                    return;
+                }
+                MetadataBuffer mb = result.getMetadataBuffer();
+                for (Metadata md : mb) {
+                    Log.d("@@@@", "meta data " + md.getTitle() + " " + md.getDriveId());
+                }
+            }
+        });
+
+        /*
+        rootFolder.createFolder(mGoogleApiClient, new MetadataChangeSet.Builder()
+                .setTitle("New folder").build());
+                */
+        /*
+        Query query = new Query.Builder()
+                .addFilter(Filters.eq(SearchableField.TITLE, "cover.jpg"))
+//                .addFilter(Filters.eq(SearchableField.MIME_TYPE, "image/jpeg"))
+                .build();
+//                .addFilter(Filters.eq(SearchableField.MIME_TYPE, "image/jpeg")).build();
+
+        Drive.DriveApi.query(mGoogleApiClient, query).setResultCallback(new ResultCallback<DriveApi.MetadataBufferResult>() {
+            @Override
+            public void onResult(DriveApi.MetadataBufferResult result) {
+                if (! result.getStatus().isSuccess()) {
+                    Log.d("@@@", "failed in retrieving messtin dir");
+                    return;
+                }
+                MetadataBuffer mb = result.getMetadataBuffer();
+                for (Metadata md : mb) {
+                    Log.d("@@@@", "meta data " + md.getTitle() + " " + md.getDriveId());
+                }
+            }
+        });
+        */
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -89,5 +186,12 @@ public class BooklistActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        if (requestCode == RESOLVE_CONNECTION_REQUEST_CODE && resultCode == RESULT_OK) {
+            mGoogleApiClient.connect();
+        }
     }
 }
