9c08e57039ee7564388c024fcb6b723f9a5fa98a
Motohiro Takayama <mootoh@gmail.com>
2014-06-03 23:56:39 +0900

    for #33: add setting activity (Android)

--- android/app/src/main/java/net/mootoh/messtin_android/app/BooklistActivity.java
+++ android/app/src/main/java/net/mootoh/messtin_android/app/BooklistActivity.java
@@ -249,6 +249,8 @@ public class BooklistActivity extends ImageHavingActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, OverallSettingsActivity.class);
+            startActivity(intent);
             return true;
         }
         return super.onOptionsItemSelected(item);
