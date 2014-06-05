c184e94f7b6339727f1b7d1acaf59c096ee3a7a1
Motohiro Takayama <mootoh@gmail.com>
2014-06-05 23:42:51 +0900

    Resolves #35: Script to replace parse.com secret info in config files.

---
+++ setup_parse.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+
+var secret = JSON.parse(fs.readFileSync('./parse_secret.json'))
+
+function replace_secrets(filename) {
+    var content = fs.readFileSync(filename).toString();
+    content = content.replace(/APP_ID_PLACEHOLDER/, secret.app_id)
+    content = content.replace(/CLIENT_KEY_PLACEHOLDER/, secret.client_key)
+    fs.writeFileSync(filename, content);
+}
+
+function replace_iOS() {
+    replace_secrets('iOS/messtin-iOS/parse_secret.plist');
+}
+
+// for Android
+function replace_android() {
+    replace_secrets('android/app/src/main/res/values/parse_secret.xml');
+}
+
+replace_iOS();
+replace_android();
