bb00e3e2118822cc0fd76c3d1393102d84323c54
Motohiro Takayama <mootoh@gmail.com>
2014-07-11 00:20:30 +0900

    Cleanup dir

--- db/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE book (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,
-    pages INTEGER NOT NULL,
-    gd_id TEXT,
-    cover_img_id TEXT,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    last_opened_at DATETIME
-);
